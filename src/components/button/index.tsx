4688552ec0fcf73ba15e8fdd46c741382ad6e9be
okyere <clement.okyere@gmail.com>
2020-07-02 13:27:00 +0000

    added material ui

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLock } from '@fortawesome/free-solid-svg-icons'
+import MatButton from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { makeStyles, Theme, createStyles } from '@material-ui/core'
+import { green } from '@material-ui/core/colors'
 
 interface IProps {
   className: string
@@ -8,17 +10,56 @@ interface IProps {
   loading?: boolean
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    wrapper: {
+      margin: theme.spacing(1),
+      position: 'relative',
+    },
+    buttonSuccess: {
+      backgroundColor: green[500],
+      '&:hover': {
+        backgroundColor: green[700],
+      },
+    },
+    fabProgress: {
+      color: green[500],
+      position: 'absolute',
+      top: -6,
+      left: -6,
+      zIndex: 1,
+    },
+    buttonProgress: {
+      color: green[500],
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      marginTop: -12,
+      marginLeft: -12,
+    },
+  })
+)
+
 export const Button: React.FC<IProps> = ({ className, loading = true, type, children }) => {
+  const classes = useStyles()
   return (
-    <React.Fragment>
-      <button className={className} type={type}>
-        {loading && (
-          <span>
-            <FontAwesomeIcon icon={faLock} />
-          </span>
-        )}
-        {children}
-      </button>
-    </React.Fragment>
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <MatButton variant="contained" color="primary" className={className} type={type}>
+          {/* {loading && (
+            <span style={{ marginRight: '5px' }}>
+              <FontAwesomeIcon icon={faSpinner} />
+            </span>
+          )} */}
+
+          {children}
+        </MatButton>
+        {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+      </div>
+    </div>
   )
 }
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import clsx from 'clsx'
 import './style/Login.css'
 import { Button } from '../../components/button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+import {
+  TextField,
+  InputAdornment,
+  makeStyles,
+  Theme,
+  createStyles,
+  InputLabel,
+  Input,
+  IconButton,
+} from '@material-ui/core'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import PasswordCircle from '@material-ui/icons/Lock'
+import Visibility from '@material-ui/icons/Visibility'
+import VisibilityOff from '@material-ui/icons/VisibilityOff'
+
+interface State {
+  password: string
+  showPassword: boolean
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    textField: {
+      width: '25ch',
+    },
+  })
+)
 
 export const LoginPage: React.FC<{}> = () => {
+  const classes = useStyles()
+  const [values, setValues] = React.useState<State>({
+    password: '',
+    showPassword: false,
+  })
+
+  const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
   return (
     <div className="container">
       <div className="sidepic_container"></div>
@@ -14,16 +61,44 @@ export const LoginPage: React.FC<{}> = () => {
             <h2 className="brand_name">Store Express</h2>
             <div className="login_items">
               <div className="login_item">
-                <span className="icon_border">
-                  <FontAwesomeIcon icon={faUser} />
-                </span>
-                <input name="username" placeholder="username or email" />
+                <TextField
+                  className={clsx(classes.margin, classes.textField)}
+                  id="username"
+                  label="Username"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircle />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
               </div>
               <div className="login_item">
-                <span className="icon_border">
-                  <FontAwesomeIcon icon={faLock} />
-                </span>
-                <input name="password" placeholder="password" />
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                  className={clsx(classes.margin, classes.textField)}
+                  id="standard-adornment-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  startAdornment={
+                    <InputAdornment position="start">
+                      <PasswordCircle />
+                    </InputAdornment>
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
               </div>
 
               <div className="login_submit">
