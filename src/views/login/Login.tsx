7735f0278fe9680a0b416d3842f23626c3f4e204
okyere <clement.okyere@gmail.com>
2020-07-02 00:02:00 +0000

    added login route

---
+++ src/views/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style/Login.css'
+
+export const LoginPage: React.FC<{}> = () => {
+  return (
+    <div className="container">
+      <div className="sidepic_container"></div>
+      <div className="login_container">
+        <div className="login_box">Hello</div>
+      </div>
+    </div>
+  )
+}
