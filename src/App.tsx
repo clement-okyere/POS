1ecf5c5c9395d1233d99da0765865f8c1feba494
okyere <clement.okyere@gmail.com>
2020-07-08 11:56:28 +0000

    added dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
 import { LoginPage } from './views/login/Login'
+import { DashboardPage } from './views/dashboard/Dashboard'
 
 const App: React.FC<{}> = () => {
   return (
     <Switch>
       <Route path="/login" component={LoginPage} />
+      <Route path="/dashboard" component={DashboardPage} />
     </Switch>
   )
 }
