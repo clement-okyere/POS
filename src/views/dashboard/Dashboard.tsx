a571eacf79ebdf268106cb46ff77a9b69e6a930c
okyere <clement.okyere@gmail.com>
2020-07-08 11:52:51 +0000

    added dashboard summary page

---
+++ src/views/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withBaseLayout } from '../../layout/Base'
+
+interface IProps {
+  appName: string
+}
+
+const Dashboard: React.FC<IProps> = ({ appName }) => {
+  return <h1>Dashboard here {appName}</h1>
+}
+
+export const DashboardaPge = withBaseLayout(Dashboard)
